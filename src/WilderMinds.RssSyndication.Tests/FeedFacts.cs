21ad23b7b98d54485b75a4f6d083f29fa7dd5f5b
Adrian Godong <adrian.godong@gmail.com>
2017-05-26 16:14:32 -0700

    Added feed deserialization function.

--- src/WilderMinds.RssSyndication.Tests/FeedFacts.cs
+++ src/WilderMinds.RssSyndication.Tests/FeedFacts.cs
@@ -5,114 +5,143 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Rfc822;
 using WilderMinds.RssSyndication;
 using Xunit;
+using DateTime = System.DateTime;
 
 namespace RssSyndication.Tests
 {
-  public class FeedFacts
-  {
-    [Fact]
-    public void FeedIsCreated()
+    public class FeedFacts
     {
-      var feed = new Feed()
-      {
-        Title = "Shawn Wildermuth's Blog",
-        Description = "My Favorite Rants and Raves",
-        Link = new Uri("http://wildermuth.com/feed"),
-        Copyright = "(c) 2016"
-      };
+        [Fact]
+        public void FeedIsCreated()
+        {
+            var feed = new Feed()
+            {
+                Title = "Shawn Wildermuth's Blog",
+                Description = "My Favorite Rants and Raves",
+                Link = new Uri("http://wildermuth.com/feed"),
+                Copyright = "(c) 2016"
+            };
 
-      Assert.NotNull(feed);
-      Assert.True(feed.Title == "Shawn Wildermuth's Blog");
-      Assert.True(feed.Description == "My Favorite Rants and Raves");
-      Assert.True(feed.Link == new Uri("http://wildermuth.com/feed"));
-      Assert.True(feed.Copyright == "(c) 2016");
+            Assert.NotNull(feed);
+            Assert.True(feed.Title == "Shawn Wildermuth's Blog");
+            Assert.True(feed.Description == "My Favorite Rants and Raves");
+            Assert.True(feed.Link == new Uri("http://wildermuth.com/feed"));
+            Assert.True(feed.Copyright == "(c) 2016");
+        }
+
+        Feed CreateTestFeed()
+        {
+            var feed = new Feed()
+            {
+                Title = "Shawn Wildermuth's Blog",
+                Description = "My Favorite Rants and Raves",
+                Link = new Uri("http://wildermuth.com/feed"),
+                Copyright = "(c) 2016"
+            };
+
+            var item1 = new Item()
+            {
+                Title = "Foo Bar",
+                Body = "<p>Foo bar</p>",
+                Link = new Uri("http://foobar.com/item#1"),
+                Permalink = "http://foobar.com/item#1",
+                PublishDate = DateTime.ParseExact(DateTime.UtcNow.ToString("r"), "r", CultureInfo.InvariantCulture), // RFC 822 / 1123 is precise down to seconds
+                Author = new Author() { Name = "Shawn Wildermuth", Email = "shawn@wildermuth.com" }
+            };
+
+            item1.Categories.Add("aspnet");
+            item1.Categories.Add("foobar");
+
+            item1.Comments = new Uri("http://foobar.com/item1#comments");
+
+            feed.Items.Add(item1);
+
+            var item2 = new Item()
+            {
+                Title = "Quux",
+                Body = "<p>Quux</p>",
+                Link = new Uri("http://quux.com/item#1"),
+                Permalink = "http://quux.com/item#1",
+                PublishDate = DateTime.ParseExact(DateTime.UtcNow.ToString("r"), "r", CultureInfo.InvariantCulture), // RFC 822 / 1123 is precise down to seconds
+                Author = new Author() { Name = "Shawn Wildermuth", Email = "shawn@wildermuth.com" }
+            };
+
+            item1.Categories.Add("aspnet");
+            item1.Categories.Add("quux");
+
+            feed.Items.Add(item2);
+
+            return feed;
+        }
+
+        [Fact]
+        public void FeedAddsItems()
+        {
+            var feed = CreateTestFeed();
+
+            Assert.NotNull(feed.Items.First());
+            Assert.True(feed.Items.First().Title == "Foo Bar");
+            Assert.True(feed.Items.ElementAt(1).Title == "Quux");
+            Assert.True(feed.Items.First().Author.Name == "Shawn Wildermuth");
+        }
+
+        [Fact]
+        public void CreatesValidRss()
+        {
+            var feed = CreateTestFeed();
+
+            var rss = feed.Serialize();
+            Debug.Write(rss);
+            var doc = XDocument.Parse(rss);
+
+            Assert.NotNull(doc);
+            var item = doc.Descendants("item").FirstOrDefault();
+            Assert.NotNull(item);
+            Assert.True(item.Element("title").Value == "Foo Bar", "First Item was correct");
+
+        }
+
+        [Fact]
+        public void DatesAreProperlyFormatted()
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
+            var feed = CreateTestFeed();
+            var rss = feed.Serialize();
+            var doc = XDocument.Parse(rss);
+            var pubDate = doc.Descendants("pubDate").First();
+
+            var rfc822FormattedDate = feed.Items.First().PublishDate.ToString("r", CultureInfo.InvariantCulture);
+            Assert.Equal(rfc822FormattedDate, pubDate.Value);
+        }
+
+        [Fact]
+        public void ParsesValidRss()
+        {
+            var expectedFeed = CreateTestFeed();
+            var xmlDocument = XDocument.Parse(expectedFeed.Serialize());
+
+            var actualFeed = Feed.Deserialize(xmlDocument);
+
+            Assert.Equal(expectedFeed.Title, actualFeed.Title);
+            Assert.Equal(expectedFeed.Link, actualFeed.Link);
+            Assert.Equal(expectedFeed.Description, actualFeed.Description);
+            Assert.Equal(expectedFeed.Copyright, actualFeed.Copyright);
+
+            Assert.Equal(expectedFeed.Items.Count, actualFeed.Items.Count);
+
+            Assert.Equal(expectedFeed.Items.First().Title, actualFeed.Items.First().Title);
+            Assert.Equal(expectedFeed.Items.First().Link, actualFeed.Items.First().Link);
+            Assert.Equal(expectedFeed.Items.First().Body, actualFeed.Items.First().Body);
+            Assert.Equal(expectedFeed.Items.First().PublishDate, actualFeed.Items.First().PublishDate);
+
+            Assert.Equal(expectedFeed.Items.Last().Title, actualFeed.Items.Last().Title);
+            Assert.Equal(expectedFeed.Items.Last().Link, actualFeed.Items.Last().Link);
+            Assert.Equal(expectedFeed.Items.Last().Body, actualFeed.Items.Last().Body);
+            Assert.Equal(expectedFeed.Items.Last().PublishDate, actualFeed.Items.Last().PublishDate);
+        }
+
     }
-
-    Feed CreateTestFeed()
-    {
-      var feed = new Feed()
-      {
-        Title = "Shawn Wildermuth's Blog",
-        Description = "My Favorite Rants and Raves",
-        Link = new Uri("http://wildermuth.com/feed"),
-        Copyright = "(c) 2016"
-      };
-
-      var item1 = new Item()
-      {
-        Title = "Foo Bar",
-        Body = "<p>Foo bar</p>",
-        Link = new Uri("http://foobar.com/item#1"),
-        Permalink = "http://foobar.com/item#1",
-        PublishDate = DateTime.UtcNow,
-        Author = new Author() { Name = "Shawn Wildermuth", Email = "shawn@wildermuth.com" }
-      };
-
-      item1.Categories.Add("aspnet");
-      item1.Categories.Add("foobar");
-
-      item1.Comments = new Uri("http://foobar.com/item1#comments");
-
-      feed.Items.Add(item1);
-
-      var item2 = new Item()
-      {
-        Title = "Quux",
-        Body = "<p>Quux</p>",
-        Link = new Uri("http://quux.com/item#1"),
-        Permalink = "http://quux.com/item#1",
-        PublishDate = DateTime.UtcNow,
-        Author = new Author() { Name = "Shawn Wildermuth", Email = "shawn@wildermuth.com" }
-      };
-
-      item1.Categories.Add("aspnet");
-      item1.Categories.Add("quux");
-
-      feed.Items.Add(item2);
-
-      return feed;
-    }
-
-    [Fact]
-    public void FeedAddsItems()
-    {
-      var feed = CreateTestFeed();
-
-      Assert.NotNull(feed.Items.First());
-      Assert.True(feed.Items.First().Title == "Foo Bar");
-      Assert.True(feed.Items.ElementAt(1).Title == "Quux");
-      Assert.True(feed.Items.First().Author.Name == "Shawn Wildermuth");
-    }
-
-    [Fact]
-    public void CreatesValidRss()
-    {
-      var feed = CreateTestFeed();
-
-      var rss = feed.Serialize();
-      Debug.Write(rss);
-      var doc = XDocument.Parse(rss);
-
-      Assert.NotNull(doc);
-      var item = doc.Descendants("item").FirstOrDefault();
-      Assert.NotNull(item);
-      Assert.True(item.Element("title").Value == "Foo Bar", "First Item was correct");
-
-    }
-
-    [Fact]
-    public void DatesAreProperlyFormatted()
-    {
-      CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
-      var feed = CreateTestFeed();
-      var rss = feed.Serialize();
-      var doc = XDocument.Parse(rss);
-      var pubDate = doc.Descendants("pubDate").First();
-
-      var rfc822FormattedDate = feed.Items.First().PublishDate.ToString("r", CultureInfo.InvariantCulture);
-      Assert.Equal(rfc822FormattedDate, pubDate.Value);
-    }
-  }
 }
--- src/WilderMinds.RssSyndication/Feed.cs
+++ src/WilderMinds.RssSyndication/Feed.cs
@@ -1,49 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Xml.Linq;
+using Rfc822;
+using DateTime = System.DateTime;
 
 namespace WilderMinds.RssSyndication
 {
-  public class Feed
-  {
-    public string Description { get; set; }
-    public Uri Link { get; set; }
-    public string Title { get; set; }
-    public string Copyright { get; set; }
-
-    public ICollection<Item> Items { get; set; } = new List<Item>();
-
-    public string Serialize()
+    public class Feed
     {
-      var doc = new XDocument(new XElement("rss"));
-      doc.Root.Add(new XAttribute("version", "2.0"));
+        public static readonly string[] Rfc822DateTimeFormatStrings =
+        {
+            "ddd, dd MMM yyyy HH':'mm':'ss 'GMT'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'UT'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'EST'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'EDT'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'CST'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'CDT'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'MST'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'MDT'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'PST'",
+            "ddd, dd MMM yyyy HH':'mm':'ss 'PDT'",
+        };
 
-      var channel = new XElement("channel");
-      channel.Add(new XElement("title", this.Title));
-      channel.Add(new XElement("link", this.Link.AbsoluteUri));
-      channel.Add(new XElement("description", this.Description));
-      channel.Add(new XElement("copyright", this.Copyright));
-      doc.Root.Add(channel);
+        public string Description { get; set; }
+        public Uri Link { get; set; }
+        public string Title { get; set; }
+        public string Copyright { get; set; }
 
-      foreach (var item in Items)
-      {
-        var itemElement = new XElement("item");
-        itemElement.Add(new XElement("title", item.Title));
-        itemElement.Add(new XElement("link", item.Link.AbsoluteUri));
-        itemElement.Add(new XElement("description", item.Body));
-        if (item.Author != null) itemElement.Add(new XElement("author", $"{item.Author.Email} ({item.Author.Name})"));
-        foreach (var c in item.Categories) itemElement.Add(new XElement("category", c));
-        if (item.Comments != null) itemElement.Add(new XElement("comments", item.Comments.AbsoluteUri));
-        if (!string.IsNullOrWhiteSpace(item.Permalink)) itemElement.Add(new XElement("guid", item.Permalink));
-        var dateFmt = item.PublishDate.ToString("r");
-        if (item.PublishDate != DateTime.MinValue) itemElement.Add(new XElement("pubDate", dateFmt));
-        channel.Add(itemElement);
-      }
+        public ICollection<Item> Items { get; set; } = new List<Item>();
 
-      return doc.ToString();
+        public static Feed GetFromXml()
+        {
+            var feed = new Feed();
+            return feed;
+        }
+
+        public string Serialize()
+        {
+            var doc = new XDocument(new XElement("rss"));
+            doc.Root.Add(new XAttribute("version", "2.0"));
+
+            var channel = new XElement("channel");
+            channel.Add(new XElement("title", this.Title));
+            channel.Add(new XElement("link", this.Link.AbsoluteUri));
+            channel.Add(new XElement("description", this.Description));
+            channel.Add(new XElement("copyright", this.Copyright));
+            doc.Root.Add(channel);
+
+            foreach (var item in Items)
+            {
+                var itemElement = new XElement("item");
+                itemElement.Add(new XElement("title", item.Title));
+                itemElement.Add(new XElement("link", item.Link.AbsoluteUri));
+                itemElement.Add(new XElement("description", item.Body));
+                if (item.Author != null) itemElement.Add(new XElement("author", $"{item.Author.Email} ({item.Author.Name})"));
+                foreach (var c in item.Categories) itemElement.Add(new XElement("category", c));
+                if (item.Comments != null) itemElement.Add(new XElement("comments", item.Comments.AbsoluteUri));
+                if (!string.IsNullOrWhiteSpace(item.Permalink)) itemElement.Add(new XElement("guid", item.Permalink));
+                var dateFmt = item.PublishDate.ToString("r");
+                if (item.PublishDate != DateTime.MinValue) itemElement.Add(new XElement("pubDate", dateFmt));
+                channel.Add(itemElement);
+            }
+
+            return doc.ToString();
+
+        }
+
+        public static Feed Deserialize(XDocument xmlDocument)
+        {
+            var feed = new Feed();
+
+            var channelNode = xmlDocument.Root?.Elements().FirstOrDefault(node => node.Name == "channel");
+            if (channelNode != null)
+            {
+                foreach (var channelChildNode in channelNode.Elements())
+                {
+                    if (channelChildNode.Name.NamespaceName != string.Empty)
+                    {
+                        continue;
+                    }
+
+                    switch (channelChildNode.Name.LocalName)
+                    {
+                        case "title":
+                            feed.Title = channelChildNode.Value;
+                            break;
+                        case "link":
+                            feed.Link = new Uri(channelChildNode.Value);
+                            break;
+                        case "description":
+                            feed.Description = channelChildNode.Value;
+                            break;
+                        case "copyright":
+                            feed.Copyright = channelChildNode.Value;
+                            break;
+                        case "item":
+                            var item = new Item();
+
+                            foreach (var itemChildNode in channelChildNode.Elements())
+                            {
+                                switch (itemChildNode.Name.LocalName)
+                                {
+                                    case "title":
+                                        item.Title = itemChildNode.Value;
+                                        break;
+                                    case "link":
+                                        item.Link = new Uri(itemChildNode.Value);
+                                        break;
+                                    case "description":
+                                        item.Body = itemChildNode.Value;
+                                        break;
+                                    case "pubDate":
+                                        item.PublishDate = new Rfc822.DateTime(itemChildNode.Value, DateTimeSyntax.FourDigitYear | DateTimeSyntax.WithDayName | DateTimeSyntax.WithSeconds | DateTimeSyntax.WithDayName).Instant.UtcDateTime;
+                                        break;
+                                }
+                            }
+
+                            feed.Items.Add(item);
+                            break;
+                    }
+                }
+            }
+
+            return feed;
+        }
 
     }
-  }
 }
